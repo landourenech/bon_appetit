81d055bf65909f76e68aeb344b04a6365c5c5f88
landourenech <116467308+landourenech@users.noreply.github.com>
2024-11-22 16:35:59 +0100

    Update route.ts

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,6 +1,6 @@
 // import bcrypt from 'bcrypt';
-import { db } from '@vercel/postgres';
-import { promos, customers, revenue, users } from '../lib/placeholder-data';
+//import { db } from '@vercel/postgres';
+//import { promos, customers, revenue, users } from '../lib/placeholder-data';
 
 // const client = await db.connect();
 
